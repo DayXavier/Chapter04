726036c3f7c39a78d8c097d169cc785fd7d583a2
Zayveeuhr <daelynxwinston@gmail.com>
2026-01-14 06:05:45 -0500

    Working with returing a value in a fuction.

--- WritingFunctions/Program.cs
+++ WritingFunctions/Program.cs
@@ -1,2 +1,14 @@
 ﻿
-TimesTable(7,255);
+//TimesTable(7,255);
+
+#region Writing a function that returns a value
+
+ConfigureConsole(culture: "fr-FR");
+
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"you must pay {taxToPay:C} in tax.");
+
+// Alternatively, call the fuction in the interpolated string.
+// WriteLine($"You must pay {CalculateTax{amount: 149, twoLetterRegionCode: "FR"):C} in tax.");
+
+#endregion
